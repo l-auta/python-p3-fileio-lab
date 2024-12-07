e346c81a0c075653ba6bf4fbdbd59946b2b9622f
lisa <lisaauta018@gmail.com>
2024-12-07 22:10:47 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt",'r') as file:
+        return file.read()
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+hi = read_file(file_name="logfile")
+print(hi)
